e338cbfa5a4525cce8d8a6a2fc1391a5270051ae
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-06-21 11:32:42 -0400

    Add SnmpV2uRequest class
    Introduce SnmpV2uRequest implementing ISnmpV2Request with Version=2. Adds Parameters (Parameters2u) and Pdu (ISnmpRequest) properties and imports SapphTools.Asn1 namespaces. Adds new file SnmpV2uRequest.cs.

---
+++ SnmpSharpNet8/Request/SnmpV2uRequest.cs
@@ -0,0 +1,10 @@
+﻿using SapphTools.Asn1;
+using SapphTools.Asn1.DataTypes;
+
+namespace SnmpSharpNet8.Request;
+
+public class SnmpV2uRequest : ISnmpV2Request {
+    public int Version => 2;
+    public Parameters2u Parameters { get; set; }
+    public ISnmpRequest Pdu { get; set; }
+}
