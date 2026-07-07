072b709246870ffd804889c28d5e0ba804ba3e9b
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-07-06 22:09:00 -0400

    Add SNMP v2/v3 ASN.1 structure definitions
    Add SnmpV2Asn1Structure and SnmpV3Asn1Structure classes to handle ASN.1 encoding/decoding of SNMP messages. These implement IAsn1Structure and manage protocol-specific data including PDU content, community strings (v2), and security parameters (v3).

---
+++ SnmpSharpNet8/Messages/SnmpAsn1Structures.cs
@@ -0,0 +1,44 @@
+﻿using SapphTools.Asn1;
+using SapphTools.Asn1.DataTypes;
+using SnmpSharpNet8.Pdu;
+using System.Formats.Asn1;
+
+namespace SnmpSharpNet8.Messages;
+
+public class SnmpV2Asn1Structure : IAsn1Structure {
+    private byte[] bytes = [];
+
+    public IReadOnlyList<IAsn1Node>? Children => [Pdu];
+    public string Community { get; init; } = "";
+    public int Length => bytes.Length;
+    public SnmpPdu Pdu { get; init; } = null!;
+    public Asn1Tag Tag => default;
+    public string? Value => string.Join(", ", Pdu.VarBindings.Select(vb => vb.Value));
+    public long Version { get; init; }
+
+    public ReadOnlySpan<byte> Raw {
+        get => bytes.AsSpan();
+        set => bytes = [.. value];
+    }
+    ReadOnlySpan<byte> IConstructable.Construct() => [];
+}
+public class SnmpV3Asn1Structure : IAsn1Structure {
+    private byte[] bytes = [];
+
+    IReadOnlyList<IAsn1Node>? IAsn1Node.Children => [ScopedPdu.InnerPdu];
+    public int Length => bytes.Length;
+    public required MsgGlobalData MsgGlobalData { get; init; }
+    public required OctetStringRaw MsgSecurityParametersEnvelope { get; init; }
+    public required UsmSecurityParameters UsmSecurityParameters { get; init; }
+    public required OctetStringRaw? ScopedPduEnvelope { get; init; }
+    public required ScopedPdu ScopedPdu { get; init; }
+    public Asn1Tag Tag => default;
+    public string? Value => string.Join(", ", ScopedPdu.InnerPdu.VarBindings.Select(vb => vb.Value));
+    public long Version { get; init; }
+
+    public ReadOnlySpan<byte> Raw {
+        get => bytes.AsSpan();
+        set => bytes = [.. value];
+    }
+    ReadOnlySpan<byte> IConstructable.Construct() => [];
+}
