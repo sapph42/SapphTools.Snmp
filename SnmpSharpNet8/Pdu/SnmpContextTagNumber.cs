79df57716e1010bedfcecf8b5bacdc2e7ce0bc8f
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-07-08 16:31:07 -0400

    Add SnmpContextTagNumber enum
    Introduce SnmpContextTagNumber enum in SnmpSharpNet8/Pdu to represent SNMP context-specific PDU tag numbers (GetRequest, GetNextRequest, GetResponse, SetRequest, GetBulkRequest, InformRequest, SnmpV2Trap, Report). Also update Asn1 submodule pointer to a newer commit.

---
+++ SnmpSharpNet8/Pdu/SnmpContextTagNumber.cs
@@ -0,0 +1,11 @@
+﻿namespace SnmpSharpNet8.Pdu;
+public enum SnmpContextTagNumber {
+    GetRequest     = 0,
+    GetNextRequest = 1,
+    GetResponse    = 2,
+    SetRequest     = 3,
+    GetBulkRequest = 5,
+    InformRequest  = 6,
+    SnmpV2Trap     = 7,
+    Report         = 8
+}
