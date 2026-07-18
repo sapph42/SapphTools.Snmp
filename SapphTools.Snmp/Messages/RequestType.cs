b1e0639b395c562b7a4fd5cd9244fe5a5588dfaa
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-07-18 00:46:28 -0400

    Add ASN.1 tag helpers and GeneralRequestType
    Make V1/V2 enums internal, add a new public GeneralRequestType that unifies request kinds, and expand V2 entries (GetBulkRequest, InformRequest, Report). Import System.Formats.Asn1 and add RequestType extension methods (ToTag) to convert enums to Asn1Tag (ContextSpecific, constructed). Restrict visibility of V1/V2 enums to internal.

--- SapphTools.Snmp/Messages/RequestType.cs
+++ SapphTools.Snmp/Messages/RequestType.cs
@@ -1,13 +1,15 @@
-﻿namespace SapphTools.Snmp.Messages; 
+﻿using System.Formats.Asn1;
 
-public enum V1RequestType {
+namespace SapphTools.Snmp.Messages;
+
+internal enum V1RequestType {
     GetRequest     = 0,
     GetNextRequest = 1,
     GetResponse    = 2,
     SetRequest     = 3,
     v1Trap         = 4
 }
-public enum V2RequestType {
+internal enum V2RequestType {
     GetRequest     = 0,
     GetNextRequest = 1,
     GetResponse    = 2,
@@ -16,4 +18,20 @@ public enum V2RequestType {
     InformRequest  = 6,
     v2Trap         = 7,
     Report         = 8
+}
+public enum GeneralRequestType {
+    GetRequest     = 0,
+    GetNextRequest = 1,
+    GetResponse    = 2,
+    SetRequest     = 3,
+    v1Trap         = 4,
+    GetBulkRequest = 5,
+    InformRequest  = 6,
+    v2Trap         = 7,
+    Report         = 8
+}
+internal static class RequestType {
+    public static Asn1Tag ToTag(this V1RequestType type) => new(TagClass.ContextSpecific, (int)type, true);
+    public static Asn1Tag ToTag(this V2RequestType type) => new(TagClass.ContextSpecific, (int)type, true);
+    public static Asn1Tag ToTag(this GeneralRequestType type) => new(TagClass.ContextSpecific, (int)type, true);
 }
