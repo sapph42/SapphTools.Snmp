37725bffa559b83e60ef9ed1152816f24e581bcd
sapph42 <31635854+sapph42@users.noreply.github.com>
2026-07-20 20:00:22 -0400

    Add DecryptionFailed exception code
    Add a new exception code to handle SNMP decryption failures, following the existing pattern of authentication and privacy-related exception codes.

--- SapphTools.Snmp/Exceptions/SnmpExceptionCode.cs
+++ SapphTools.Snmp/Exceptions/SnmpExceptionCode.cs
@@ -6,6 +6,7 @@ public enum SnmpExceptionCode {
     UnsupportedNoAuthPriv,
     InvalidAuthenticationParameterLength,
     AuthenticationFailed,
+    DecryptionFailed,
     UnsupportedPrivacyProtocol,
     InvalidPrivacyParameterLength,
     InvalidAuthoritativeEngineId,
